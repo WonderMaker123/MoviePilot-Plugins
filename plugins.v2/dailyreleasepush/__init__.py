60fab70b25413e28c7bc8707079c8c5c7220c385
plsy1 <chisan1230@protonmail.com>
2024-12-01 02:42:15 +0800

    .

--- plugins.v2/dailyreleasepush/__init__.py
+++ plugins.v2/dailyreleasepush/__init__.py
@@ -21,7 +21,7 @@ class DailyReleasePush(_PluginBase):
     # 插件图标
     plugin_icon = "statistic.png"
     # 插件版本
-    plugin_version = "0.3.1"
+    plugin_version = "0.3.2"
     # 插件作者
     plugin_author = "plsy1"
     # 作者主页
